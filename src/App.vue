9306eb0bd3d7fc5e5ea90e74b5e07302b08317bb
Millie Lin <millie.lin@britishcouncil.org.hk>
2018-08-27 18:18:45 +0800

    Git added axio and scss and space X data.

--- src/App.vue
+++ src/App.vue
@@ -16,13 +16,5 @@ export default {
 }
 </script>
 
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+<style src="./styles/app.scss" lang="scss">
 </style>
