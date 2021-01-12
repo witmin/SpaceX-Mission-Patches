4a248ff453b818ee17186db9780c495fcf1a34d9
Millie Lin <linxinmin@gmail.com>
2021-01-12 14:21:18 +0800

    Hide mission image if null

--- src/components/MissionPatches.vue
+++ src/components/MissionPatches.vue
@@ -1,44 +1,44 @@
 <template>
+  <div>
+    <h1>{{ title }}</h1>
+    <p class="meta">Through SpaceX data API</p>
     <div>
-        <h1>{{ title }}</h1>
-        <p class="meta">Through SpaceX data API</p>
-        <div>
-            <ul class="mission-patches-list">
-                <li v-for="mission in missions" :key="mission.flight_number">
-                    <img :src="mission.links.mission_patch" :alt="mission.mission_name" class="mission-patch">
-                    <p class="launch-year">{{mission.launch_year}}</p>
-                  <h2 class="mission-name">{{mission.mission_name}}</h2>
-                </li>
-            </ul>
-        </div>
+      <ul class="mission-patches-list">
+        <li v-for="mission in missions" :key="mission.flight_number">
+          <img :src="mission.links.mission_patch" :alt="mission.mission_name" class="mission-patch" v-show="mission.links.mission_patch !== null">
+          <p class="launch-year">{{ mission.launch_year }}</p>
+          <h2 class="mission-name">{{ mission.mission_name }}</h2>
+        </li>
+      </ul>
     </div>
+  </div>
 </template>
 
 <script>
-    import axios from 'axios';
+import axios from 'axios';
 
-    export default {
-        name: 'MissionPatches',
-        props: {
-            msg: String
-        },
-        data() {
-            return {
-                title: 'SpaceX Mission Patches',
-                info: null,
-                missions: null
-            }
-        },
-        mounted() {
-            axios
-                .get('https://api.spacexdata.com/v2/launches')
-                .then(response => (this.info = response, this.missions = response.data))
-                .catch(e => {
-                    this.errors.push(e)
-                })
-
-        }
+export default {
+  name: 'MissionPatches',
+  props: {
+    msg: String
+  },
+  data() {
+    return {
+      title: 'SpaceX Mission Patches',
+      info: null,
+      missions: null
     }
+  },
+  mounted() {
+    axios
+        .get('https://api.spacexdata.com/v2/launches')
+        .then(response => (this.info = response, this.missions = response.data))
+        .catch(e => {
+          this.errors.push(e)
+        })
+
+  }
+}
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
